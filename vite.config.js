76f9bab82972a7d69bb25af4a51ff903134e9b2d
kasim-kc <kasimsactive1@gmail.com>
2025-07-23 16:41:18 +0530

    fix

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,14 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    headers: {
+      "Content-Type": "application/javascript",
+    },
+  },
   resolve: {
-    extensions: [".js", ".jsx"],
+    extensions: [".js", ".jsx", ".ts", ".tsx"],
   },
 });
