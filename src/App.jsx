14c63562ed70439bccf3e7b6d8d18e9345c5a5c5
kasim-kc <kasimsactive1@gmail.com>
2025-07-23 08:16:13 +0530

    fixed the unwanted bottom-margin, replaced it top

--- src/App.jsx
+++ src/App.jsx
@@ -5,15 +5,13 @@ import "./App.css";
 
 function App() {
   return (
-    <>
-      <div className="main-container">
-        <Header />
-        <DescSection descPresent={true} education={false} />
-        <DescSection descPresent={false} education={true} />
-        <CardSection smCard={true} bigCard={false} />
-        <CardSection bigCard={true} smCard={false} />
-      </div>
-    </>
+    <div className="main-container">
+      <Header />
+      <DescSection descPresent={true} education={false} />
+      <DescSection descPresent={false} education={true} />
+      <CardSection smCard={true} bigCard={false} />
+      <CardSection bigCard={true} smCard={false} />
+    </div>
   );
 }
 
